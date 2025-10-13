a239c098d2fe14078511512a0d2880d8c2fe9ff7
sunny91100 <matteoproust18@gmail.com>
2025-10-13 11:36:30 +0200

    navbar

---
+++ navbar/sidebar.js
@@ -0,0 +1,7 @@
+    function openSidebar(id) {
+      document.getElementById(id).classList.add('open');
+    }
+
+    function closeSidebar(id) {
+      document.getElementById(id).classList.remove('open');
+    }
